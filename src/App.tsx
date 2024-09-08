00adf53024a33addc8d8f1daa3f58c580928b824
Manjunath s <manjunathadvg2002@gmail.com>
2024-09-08 22:15:51 +0530

    updates

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App: React.FC=()=>{
+ 
+
+  return (
+    <>
+      <h3>Manjunath s</h3>
+      <p>I AM COMING FROM THE CONFIG ex</p>
+    </>
+  )
+}
+
+export default App
